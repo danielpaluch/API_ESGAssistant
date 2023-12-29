a1c07593ffe13f32c9b83547387583d70617a32c
Kamil <hadekshd@gmail.com>
2023-12-29 23:05:49 +0100

    fix: importing user modules

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService, ConfigModule],
+  providers: [UserService, UserResolver, ConfigModule],
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    ConfigModule.forRoot({
-      cache: true,
-    }),
   ],
   exports: [UserService],
 })
